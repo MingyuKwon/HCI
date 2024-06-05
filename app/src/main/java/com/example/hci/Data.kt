ccdca451138addf019cf20c7348fac56dcd2c90d
Kwon Mingyu <kmgmg230601@gmail.com>
2024-06-05 14:26:08 +0900

    데이터 공유할 떄 쓸 data 클래스 생성

---
+++ app/src/main/java/com/example/hci/Data.kt
@@ -0,0 +1,11 @@
+package com.example.hci
+
+import com.google.android.gms.maps.model.LatLng
+
+class Data {
+
+    companion object {
+        var currentLocationLatng : LatLng? = null
+        var currentLocationAddress : String? = null
+    }
+}
