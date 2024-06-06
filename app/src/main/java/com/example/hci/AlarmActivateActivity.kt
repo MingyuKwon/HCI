27ef02be20ed72c9e1231c011ef5618a3c7a223a
Kwon Mingyu <kmgmg230601@gmail.com>
2024-06-07 00:02:54 +0900

    알림 활성화 창에서 목적지랑 현재 위치 동시에 보여주도록 했습니다. 특히 현재위치는 위치가 갱신 될 떄 마다 계속 바뀝니다

--- app/src/main/java/com/example/hci/AlarmActivateActivity.kt
+++ app/src/main/java/com/example/hci/AlarmActivateActivity.kt
@@ -1,12 +1,49 @@
 package com.example.hci
 
+import android.Manifest
+import android.app.AlertDialog
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Geocoder
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import android.os.Bundle
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.fragment.app.FragmentActivity
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.Marker
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.Locale
 
-class AlarmActivateActivity : AppCompatActivity() {
+class AlarmActivateActivity : FragmentActivity(), OnMapReadyCallback {
+
+
+    private lateinit var gMap: GoogleMap
+    private lateinit var map: FrameLayout
+    private lateinit var locationManager: LocationManager
+    private lateinit var loadingDialog: AlertDialog
+
+    private val LOCATION_PERMISSION_REQUEST_CODE = 1
+
+    private var currentMarker: Marker? = null
+
+    private var isInitalLocationGetted: Boolean = false
+
+    private var _currentLatLng : LatLng? = null
+    private var _currentAddress : String? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +53,125 @@ class AlarmActivateActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        map = findViewById(R.id.alarmMap)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.alarmMap) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+
+        createLoadingDialog() // 로딩 다이얼로그 생성
+        showLoadingDialog() // 초기 로딩 다이얼로그 표시
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        gMap = googleMap
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION), LOCATION_PERMISSION_REQUEST_CODE)
+        } else {
+            enableMyLocation()
+        }
+    }
+
+    private fun createLoadingDialog() {
+        val builder = AlertDialog.Builder(this)
+        builder.setView(layoutInflater.inflate(R.layout.dialog_loading, null))
+        builder.setCancelable(false) // 사용자가 다이얼로그를 닫을 수 없도록 설정
+        loadingDialog = builder.create()
+    }
+    private fun showLoadingDialog() {
+        loadingDialog.show()
+    }
+
+    private fun hideLoadingDialog() {
+        loadingDialog.dismiss()
+    }
+
+    private fun enableMyLocation() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED ||
+            ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0L, 0f, locationListener)
+            locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0L, 0f, locationListener)
+
+            val lastKnownLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+                ?: locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER)
+
+            lastKnownLocation?.let {
+                val currentLatLng = LatLng(it.latitude, it.longitude)
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+                getAddressFromLatLng(currentLatLng) // 위도와 경도로부터 주소 가져오기
+            }
+
+        }
+    }
+
+    private fun getAddressFromLatLng(latLng: LatLng) : String? {
+        val geocoder = Geocoder(this, Locale.getDefault())
+        try {
+            val addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1)
+            if (addresses != null && addresses.isNotEmpty()) {
+                val address = addresses[0].getAddressLine(0)
+                return address
+            }
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+
+        return null
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == LOCATION_PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                enableMyLocation()
+            }
+        }
+    }
+
+    private fun updateMarker(latLng: LatLng) {
+        if (!isInitalLocationGetted ) {
+            gMap.addMarker(
+                MarkerOptions()
+                    .position(Data.DestinationLocationLatng!!)
+                    .title("Destination Location")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_BLUE))
+            )
+            isInitalLocationGetted = true
+            hideLoadingDialog()
+        }
+
+        if(currentMarker == null)
+        {
+            gMap.addMarker(
+                MarkerOptions()
+                    .position(latLng)
+                    .title("Current Location")
+                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)) // 마커 색상 설정
+            )
+        }else
+        {
+            currentMarker?.position = latLng
+        }
+    }
+
+    private val locationListener = object : LocationListener {
+        override fun onLocationChanged(location: Location) {
+            val currentLatLng = LatLng(location.latitude, location.longitude)
+            if(!isInitalLocationGetted)
+            {
+                gMap.moveCamera(CameraUpdateFactory.newLatLngZoom(currentLatLng, 15f))
+            }
+            updateMarker(currentLatLng)
+            getAddressFromLatLng(currentLatLng) // 위도와 경도로부터 주소 가져오기
+        }
+
+        override fun onStatusChanged(provider: String, status: Int, extras: Bundle) {}
+        override fun onProviderEnabled(provider: String) {}
+        override fun onProviderDisabled(provider: String) {}
     }
 }
